4eec72e2f0757142dfde5b335c2feda7804f309c
Saikat Maji <majisaikat864@gmail.com>
2025-02-15 20:59:26 +0530

    news.js edited

--- src/components/News.js
+++ src/components/News.js
@@ -30,7 +30,7 @@ export class News extends Component {
       page: 1,
       totalResults: 0
     }
-    document.title = `${this.capitalizeFirstLetter(this.props.category)}-NewsMonkey`;
+    document.title = `${this.capitalizeFirstLetter(this.props.category)}-NewsApp`;
   }
   async updateNews() {
     this.props.setProgress(10);
